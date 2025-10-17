9e4152d251ea8281b25d431c9861bc7f39013509
Edupe2603 <31553111+Edupe2603@users.noreply.github.com>
2025-10-17 23:51:50 +0000

    Primer commit Landing Page

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+
+        <div className="container-fluid justify-content-center" style={{ width: "80%" }}>
+        <footer className="bg-dark text-center text-white">
+            <div>
+                <h3>Footer Content</h3>
+            </div>
+        </footer>
+        </div>
+    );
+}
+export default Footer;
